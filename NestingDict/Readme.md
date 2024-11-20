5f980a9e371d0e1cc9a3dfa0ff5004b54f14c608
Nibesh7 <ranjitnibesh7@gmail.com>
2024-11-20 19:35:50 +0545

    projects

---
+++ NestingDict/Readme.md
@@ -0,0 +1,9 @@
+simple_dictionary = {
+    key: value,
+    key1: vaue
+}
+# Nesting
+nesting = {
+    key: [list],
+    key1: {dict},
+} 
---
+++ NestingDict/nesting.py
@@ -0,0 +1,28 @@
+capitals = {
+    "Nepal" : "Katmandu",
+    "Germany": "Berlin",
+    "France": "Paris"
+}
+
+#Nesting List in Dictionary
+# travel_log = {
+#     "France": ['paris', 'dijon', 'lille'],
+#     "Nepal": ['ktm', 'pokhara', 'barhanise'],
+#     "Germany": ['Berlin', 'stuggart', 'hamurg'],
+# }
+
+# Nesting Dictionary in Dicitonary
+# travel_log = {
+#     "France": {"citites_visited" : ['paris', 'dijon', 'lille'] , "total_visits": '12' },
+#     "Nepal": {"citites_visited" : ['ktm', 'pokhara', 'barhanise'],  "total_visits": '122' },
+#     "Germany": {"citites_visited" :['Berlin', 'stuggart', 'hamurg'], "total_visits": '2'},
+# }
+
+
+#Nesting A dictionary in a list
+
+travel_log = [
+    {"country": "France", "citites_visited" : ['paris', 'dijon', 'lille'] , "total_visits": '12' },
+    {"country": "Nepal", "citites_visited" : ['ktm', 'pokhara', 'barhanise'],  "total_visits": '122' },
+    {"country": "Germany", "citites_visited" :['Berlin', 'stuggart', 'hamurg'], "total_visits": '2'},
+]
